35aa2b662eb04a7e283bf0cf5df09eb53b5bdac4
carlos <crlos.rjas.2001@gmail.com>
2023-09-01 19:02:08 -0500

    responsive

--- src/recursos/Footer.js
+++ src/recursos/Footer.js
@@ -30,7 +30,16 @@ export const Footer = () => {
                  <div className='love'></div>
                  <div className='llovr'></div>
                  <div className='llover'></div>
-               
+                 <div className='ultims_palabras'>
+                <p className='ya'>Me considero una persona muy trabajadora, aprendo rápido
+                   cuando se trata de entender y aprender nuevas cosas, práctico constantemente lo que se me enseña
+                     y tengo una actitud positiva con respecto al trabajo, mis ganas de
+                     aprender es el resultado de ello. En consecuencia, de todo aquello voy a obtener un certificado de la 
+                     empresa ORACLE.
+                      Mi actitud me ha abierto muchas puertas de trabajo que en su momento
+                      necesite, pero con la experiencia que voy adquiriendo aspiro a más, con mi trabajo, eficiencia y
+                      sacrificio perfeccionare mis habilidades ya qué será un gusto trabajar para esta empresa .</p>
+              </div>
                  
 
                 
@@ -59,17 +68,7 @@ export const Footer = () => {
               <div className='t'></div>
               <div className='bbb'></div>
 
-              <div className='ultims_palabras'>
-                <p className='ya'>Me considero una persona muy trabajadora, aprendo rápido
-                   cuando se trata de entender y aprender nuevas cosas, práctico constantemente lo que se me enseña
-                     y tengo una actitud positiva con respecto al trabajo, mis ganas de
-                     aprender es el resultado de ello.En consecuencia de todo aquello voy a obtener un certificado de la 
-                     empresa ORACLE.
-                      Mi actitud me ha abierto muchas puertas de trabajo que en su momento
-                      necesite pero con la experiencia que voy adquiriendo aspiro a más, con mi trabajo, eficiencia y
-                      sacrificio perfeccionare mis abilidades ya qué sera un gusto trabajar para esta empresa
-                      .Gracias y que Dios los vendiga</p>
-              </div>
+              
               
     </div>
   )
